a576dda6726c65d78777be771afac2710087ea88
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-02-24 01:26:11 +0530

    Create ColorCode.h
    Added a header file for function declaration, structure definition and defined the Major and minor colors

---
+++ ColorCode.h
@@ -0,0 +1,39 @@
+//header file
+
+//Define the Major and Minor colors used in the transmission lines
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair; 
+
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+//Calculate number of Major colors
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+
+const char* MinorColorNames[] = 
+{
+   "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+//Calculate number of minor colors
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+const int MAX_COLORPAIR_NAME_CHARS = 25;
+const int MIN_COLORPAIR_NAME_CHARS = 1;
+
+//function declarations
+
+ColorPair GetColorFromPairNumber(int);
+void ColorPairToString(const ColorPair* ExtractColorPair, char* buffer);
+void testNumberToPair(int, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+int GetPairNumberFromColor(const ColorPair* PairNumberfromColor );
+void testPairToNumber(enum MajorColor RequestedMajor, enum MinorColor RequestedMinor, int);
+void reference_manual();
