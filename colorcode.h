5e17061bc89483f4917c74450e0f59db72ffb5f7
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-02-24 12:08:25 +0530

    Update colorcode.h
    extern keyword added

--- colorcode.h
+++ colorcode.h
@@ -4,36 +4,36 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-typedef struct {
+extern typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair; 
 
 
-const char* MajorColorNames[] = {
+extern const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
 //Calculate number of Major colors
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+extern int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 
-const char* MinorColorNames[] = 
+extern const char* MinorColorNames[] = 
 {
    "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
 //Calculate number of minor colors
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+extern int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-const int MAX_COLORPAIR_NAME_CHARS = 25;
-const int MIN_COLORPAIR_NAME_CHARS = 1;
+extern const int MAX_COLORPAIR_NAME_CHARS = 25;
+extern const int MIN_COLORPAIR_NAME_CHARS = 1;
 
 //function declaration
 
-ColorPair GetColorFromPairNumber(int);
-void ColorPairToString(const ColorPair* ExtractColorPair, char* buffer);
-void testNumberToPair(int, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-int GetPairNumberFromColor(const ColorPair* PairNumberfromColor );
-void testPairToNumber(enum MajorColor RequestedMajor, enum MinorColor RequestedMinor, int);
-void reference_manual();
+extern ColorPair GetColorFromPairNumber(int);
+extern void ColorPairToString(const ColorPair* ExtractColorPair, char* buffer);
+extern void testNumberToPair(int, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+extern int GetPairNumberFromColor(const ColorPair* PairNumberfromColor );
+extern void testPairToNumber(enum MajorColor RequestedMajor, enum MinorColor RequestedMinor, int);
+extern void reference_manual();
